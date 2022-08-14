2b901477de89fe7a12fa0aa3e467a18f178a47d7
Filip Trplan <filip@trplan.si>
2022-08-14 11:02:28 +0200

    custom events

--- template/event_handler.py
+++ template/event_handler.py
@@ -5,11 +5,25 @@ import pygame
 class EventHandler():
     def __init__(self, game_manager: GameManager):
         self.game_manager = game_manager
+        self.post_test_event()
 
     def handle_event(self, event: pygame.event.Event):
-        if event.type == CUSTOM_EVENT:
-            # do stuff
-            print('hello')
-        elif event.type == CUSTOM_EVENT2:
-            print('hello2')
-            # do some other stuff
+
+        # look to post_test_event for how to trigger custom events
+        if event.type == pygame.USEREVENT:
+            if event.custom_type == CUSTOM_EVENT:
+                # do stuff
+                print(event.message)
+            elif event.custom_type == CUSTOM_EVENT2:
+                # do some other stuff
+                print('hello2')
+
+    def post_test_event(self):
+        # custom_type is defined in constants.py and message can be anything
+        my_event = pygame.event.Event(pygame.USEREVENT, custom_type=CUSTOM_EVENT, message='Hello')
+
+        # post an event right now
+        pygame.event.post(my_event)
+
+        # set a timer
+        pygame.time.set_timer(my_event, 1000)
